6ce76858b97d31c1eaa9ff258c9f6d96c313b8b6
Daniel <monobootica@yandex.ru>
2021-02-16 23:09:17 +0500

    task 3 DOM is complete

---
+++ hamburger/app.js
@@ -0,0 +1,12 @@
+class Hamburger {
+    constructor (size, stuffing) {
+        size = document.querySelector()
+    }
+    addTopping(topping) {} // Добавить добавку }
+        removeTopping(topping) {} // Убрать добавку }
+            getToppings(topping) {} // Получить список добавок }
+                getSize() {} // Узнать размер гамбургера }
+                    getStuffing() {} // Узнать начинку гамбургера }
+                        calculatePrice() {} // Узнать цену }
+                            calculateCalories() {} // Узнать калорийность }
+                            }
