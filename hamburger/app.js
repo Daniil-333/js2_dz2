ca129389e8ef169b8adf2d2e8cf0de86d4229970
Daniel <monobootica@yandex.ru>
2021-02-18 13:47:19 +0500

    task 3 try to change calculateCalories

--- hamburger/app.js
+++ hamburger/app.js
@@ -11,7 +11,7 @@ class Hamburger {
     constructor (size, stuffing) {
         this.size = this.getSize();
         this.stuffing = this.getStuffing();
-        this.renderCost();
+        this.renderResult();
     }
     addTopping(topping) {} // Добавить добавку
     removeTopping(topping) {} // Убрать добавку
@@ -42,25 +42,44 @@ class Hamburger {
     } // Узнать цену
     calculateCalories() {
         let totalCallory = null;
-        for (let i = 0; i < this.dataSize.length; i++) {
-            if (this.size === this.dataSize[i].title) {
+        for (let i = 0; i < this.dataSize.length || i < this.dataStuffing.length; i++) {
+            if (this.size === this.dataSize[i].title || this.stuffing === this.dataStuffing[i].title) {
                 return totalCallory += this.dataSize[i].cal;
             }
         }
-        for (let i = 0; i < this.dataStuffing.length; i++) {
-            if (this.stuffing === this.dataStuffing[i].title) {
-                return totalCallory += this.dataStuffing[i].cal;
-            }
-        }
-        return totalCallory;
+        // for (let i = 0; ; i++) {
+        //     if () {
+        //         return totalCallory += this.dataStuffing[i].cal;
+        //     }
+        // }
+        // return totalCallory;
     } // Узнать калорийность
-    renderCost() {
-        return price.textContent = `Стоимость гамбургера ${this.calculatePrice()} рублей`
-    } // Отображение разметки стоимости
-    renderCal() {} // Отображение разметки калорийности
+    renderResult() {
+        return price.textContent = `Стоимость гамбургера ${this.calculatePrice()} рублей. Каллоийность ${this.calculateCalories()} каллорий`
+    } // Отображение разметки стоимости и каллорий
 }
 let btnCost = document.querySelector('.mainForm__btn-cost');
 let price = document.querySelector('.total__price');
-btnCost.addEventListener('click', () => {
+btnCost.addEventListener('click', (event) => {
+    event.preventDefault();
     let hamburger = new Hamburger(this.size, this.stuffing);
 });
+
+//Ниже по тексту Черновик!
+data = [
+    {title: 'big', price: 100, cal: 40},
+    {title: 'small', price: 50, cal: 20},
+    {title: 'cheese', price: 10, cal: 20},
+    {title: 'salad', price: 20, cal: 45},
+    {title: 'potato', price: 15, cal: 10}
+];
+
+let totalCallory = null;
+data.forEach(function (obj, title) {
+    if (this.size === obj.title) {
+        return totalCallory += obj.cal;
+    }
+});
+
+
+
